d6c2e3d058b768fba1e5e86688e7b60cf33ee6f1
frztr <frztr@yandex.ru>
2025-04-05 09:55:55 +0300

    калька курсача dev

--- CW_OOP/Modules/Role/Service/IService.cs
+++ CW_OOP/Modules/Role/Service/IService.cs
@@ -2,19 +2,6 @@ using Role.DTO;
 
 namespace Role;
 
-// public interface IService
-// {
-//     public IEnumerable<DTO.EntityDto> GetAll();
-
-//     public DTO.EntityDto GetById(short id);
-
-//     public void Add(DTO.AddDto addDto);
-
-//     public void Delete(short id);
-
-//     public void Update(DTO.UpdateDto updateDto);
-// }
-
 public interface IService : IBaseService<short,AddDto,UpdateDto,EntityDto>
 {
 }
--- CW_OOP/Modules/Role/Service/Service.cs
+++ CW_OOP/Modules/Role/Service/Service.cs
@@ -3,34 +3,6 @@ using Role.DTO;
 
 namespace Role;
 
-// public class Service(IRepository repository) : IService
-// {
-//     public void Add(DTO.AddDto addDto)
-//     {
-//         repository.Add(addDto);
-//     }
-
-//     public void Delete(short id)
-//     {
-//         repository.Delete(id);
-//     }
-
-//     public IEnumerable<DTO.EntityDto> GetAll()
-//     {
-//         return repository.GetAll();
-//     }
-
-//     public DTO.EntityDto GetById(short id)
-//     {
-//         return repository.GetById(id);
-//     }
-
-//     public void Update(DTO.UpdateDto updateDto)
-//     {
-//         repository.Update(updateDto);
-//     }
-// }
-
 public class Service : BaseService<short, AddDto, EntityDto, UpdateDto, AddDto, EntityDto, UpdateDto>, IService
 {
     public Service(IBaseRepository<short, AddDto, UpdateDto, EntityDto> repository) : base(repository)
