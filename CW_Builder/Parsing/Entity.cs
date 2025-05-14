dfe20eefafb5bcb287ae6ebb092fe5333f77648c
frztr <frztr@yandex.ru>
2025-05-14 15:52:29 +0300

    CW_Builder удалён, т.к. перемещен в отдельную ветку

--- CW_Builder/Parsing/Entity.cs
+++
@@ -1,8 +0,0 @@
-public class Entity
-{
-    public string Name { get; set; }
-
-    public List<EntityProp> Props { get; set; } = new List<EntityProp>();
-
-    public bool ExtendedSearch { get; set; } = false;
-}
